28e820b9f2da3ccb0783a4d7464ba77a9dc18ad8
Lisa Miner <midnightmoet@gmail.com>
2023-08-27 10:08:24 -0400

    Completed, could change logo later

--- script.js
+++ script.js
@@ -11,10 +11,24 @@ function updateClock(){
     setTimeout(updateClock, 1000); // run every second
     //console.log(currentDate); // test
 
-    const hour = currentDate.getHours();
-    const minute = currentDate.getMinutes();
-    const second = currentDate.getSeconds();
+    const hour = currentDate.getHours(); // 0-23
+    const minute = currentDate.getMinutes(); // 0-59
+    const second = currentDate.getSeconds(); // 0-59
     //console.log(hour, minute, second); // test
+
+    const hourDeg = (hour / 12) * 360; // 360 degrees in a circle
+    hourElement.style.transform = `rotate(${hourDeg}deg)`;
+    //console.log(hourDeg); // test
+
+    const minuteDeg = (minute / 60) * 360; // 360 degrees in a circle
+    minuteElement.style.transform = `rotate(${minuteDeg}deg)`;
+    //console.log(minuteDeg); // test
+
+    const secondDeg = (second / 60) * 360; // 360 degrees in a circle
+    secondElement.style.transform = `rotate(${secondDeg}deg)`;
+    //console.log(secondDeg); // test
 }
 
-updateClock(); // run once on page load
+// updateClock(); // run once on page load
+
+setInterval(updateClock, 1000); // run every second
